722a2b194791208498020bfa37d792cf2c02df63
Johan <121931753+JohanJoubertSybrin@users.noreply.github.com>
2023-01-24 10:28:23 +0200

    02-09

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -6,9 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent {
-  name = 'The Redemption';
-  wasWatched(){
-    return true;
-  }
-}
+export class MediaItemComponent {}
