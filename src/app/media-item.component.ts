cbe97bc6ffd3a97241c9ae52b1de75109a05e908
Johan <121931753+JohanJoubertSybrin@users.noreply.github.com>
2023-01-24 10:31:23 +0200

    02-10

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+  onDelete(){
+    console.log('Press delete')
+  }
+}
